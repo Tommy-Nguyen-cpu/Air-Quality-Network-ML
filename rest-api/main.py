4038f3c7c038653b5c6575c43b705dcb17eefa27
issacatwit <issac@wit.edu>
2023-12-07 22:23:37 -0500

    fix df from csv

--- rest-api/main.py
+++ rest-api/main.py
@@ -3,21 +3,32 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 
-import tensorflow as tf
-from tensorflow import keras
+# import tensorflow as tf
+# from tensorflow import keras
+import pandas as pd
 
 import re
+from io import StringIO
 
 app = Flask(__name__)
 CORS(app, support_credentials=True)
 
-model = tf.keras.models.load_model('./Air Quality ML/AirQModel.h5')
+# model = tf.keras.models.load_model('../Air Quality ML/AirQModel.h5')
 
 class File:
-    def __init__(self, filename, data):
+    def __init__(self, filename, cols, data):
         self.filename = filename
+        self.cols = cols
         self.data = data
-
+        self.getDF()
+    
+    def getDF(self):
+        self.df = pd.read_csv(
+            StringIO('\n'.join(self.data)),
+            sep = ',',
+            header = None,
+            names = self.cols,
+        )
 
 @app.route('/upload_csv', methods = ['POST'])
 @cross_origin(supports_credentials=True)
@@ -33,10 +44,9 @@ def upload_csv():
         lines = file.split('\n')
         headers = lines[1]
         file_name = re.findall('"([^"]*)"', headers)[1]
-        data = lines[4:-2]
-        files.append(File(file_name, data))
-
-    print(files[0].data[0])
+        cols = lines[4].split(',')
+        data = lines[5:-2]
+        files.append(File(file_name, cols, data))
 
     return jsonify({'success': True})
 
